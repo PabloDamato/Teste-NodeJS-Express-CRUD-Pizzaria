235886e71ef75e58eb19afeb559385df40e44c49
Pablo Gomes <pablodamato6@gmail.com>
2023-07-31 18:46:57 -0300

    Fix renomeando as rotas

--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -2,13 +2,13 @@ const router = require("express").Router();
 const usuario = require("../controller/usuario.controller.js");
 
 //rotas GET
-router.get("/findAll", usuario.findAll);
-router.get("/find/:nome", usuario.find);
+router.get("/findAll", usuario.findAllController);
+router.get("/find/:nome", usuario.findController);
 //rotas POST
-router.post("/create", usuario.createUsuario);
+router.post("/create", usuario.createUsuarioController);
 //rotas PUT
-router.put("/update/:nome", usuario.updateUsuario);
+router.put("/update/:nome", usuario.updateUsuarioController);
 //rotas DELETE
-router.delete("/delete/:nome", usuario.deleteUsuario);
+router.delete("/delete/:nome", usuario.deleteUsuarioController);
 
 module.exports = router;
