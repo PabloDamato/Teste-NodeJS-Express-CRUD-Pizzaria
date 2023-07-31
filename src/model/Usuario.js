0c4dcbe7b178fcabddcc3c58dac58ff1722b06cb
Pablo Gomes <pablodamato6@gmail.com>
2023-07-31 18:18:04 -0300

    Feat: Atualizado o model do usuario

--- src/model/Usuario.js
+++ src/model/Usuario.js
@@ -4,7 +4,6 @@ const UsuarioSchema = new mongoose.Schema({
     nome: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     senha: { type: String },
-    imagem: { type: String, required: true },
     logradouro: {
         rua: { type: String },
         numero: { type: Number },
@@ -13,14 +12,8 @@ const UsuarioSchema = new mongoose.Schema({
         pais: { type: String }
     },
     telefone: { type: Number },
-    createdAt: { type: Date, required: true },
-    produtos_fav: [
-        {
-            _id: { type: mongoose.Schema.Types.ObjectId, required: true, unique: true, ref: "produtos" },
-            createdAt: { type: Date, required: true },
-        }
-    ],
     admin: { type: Boolean, required: true, default: false },
+    createdAt: { type: Date, required: true },
 });
 
 const Usuario = mongoose.model("usuarios",UsuarioSchema);
