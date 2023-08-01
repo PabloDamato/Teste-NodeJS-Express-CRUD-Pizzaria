2a949d0bb68261a5cb821f7d022b6bf1c3f1ed97
Pablo Gomes <pablodamato6@gmail.com>
2023-07-31 21:40:07 -0300

    Feat: criptografia e required no user

--- src/model/Usuario.js
+++ src/model/Usuario.js
@@ -1,19 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UsuarioSchema = new mongoose.Schema({
     nome: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    senha: { type: String },
+    senha: { type: String, required: true},
     logradouro: {
-        rua: { type: String },
-        numero: { type: Number },
-        cidade: { type: String },
-        estado: { type: String },
-        pais: { type: String }
+        rua: { type: String, required: true },
+        numero: { type: Number, required: true },
+        cidade: { type: String, required: true },
+        estado: { type: String, required: true },
+        pais: { type: String, required: true }
     },
-    telefone: { type: Number },
+    telefone: { type: Number, required: true },
     admin: { type: Boolean, required: true, default: false },
     createdAt: { type: Date, required: true, default: Date.now() },
+});
+
+UsuarioSchema.pre("save", async function(next){
+    if (this.senha) {
+        this.senha = await bcrypt.hash(this.senha, 10);
+    }
+    next();
 });
 
 const Usuario = mongoose.model("usuarios",UsuarioSchema);
