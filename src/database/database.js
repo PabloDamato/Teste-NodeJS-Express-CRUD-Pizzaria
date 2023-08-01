fafa878ceb6264e94cc7fb86435e10578faa1cd4
Pablo Gomes <pablodamato6@gmail.com>
2023-07-31 21:02:08 -0300

    Fix: alterando loca DB

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function connectToDatabase() {
-    mongoose.connect('mongodb://127.0.0.1:27017/crud', {
+    mongoose.connect('mongodb://127.0.0.1:27017/crud-pizzaria', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(() => {
